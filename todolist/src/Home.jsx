6432ad4544f49841ef58fdd2ec061034bab62a43
Joe Wigmar <joewigmar@gmail.com>
2025-10-27 14:05:12 -0300

    feat: create Create.jsx component and import it into Home.jsx

--- todolist/src/Home.jsx
+++ todolist/src/Home.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import Create from "./Create";
 
 function Home() {
+    const [todos, setTodos] = useState([])
     return (
-        <div>Todo List</div>
+        <div>
+            <h2>Todo List</h2>
+            <Create />
+            {
+                todos.length === 0 
+                ?
+                <div><h2>No Record</h2></div>
+                :
+                todos.map(todo => (
+                    <div>
+                        {todo}
+                    </div>
+            
+                ))
+            }
+        </div>
     )
 }
 
