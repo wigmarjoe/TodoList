458a44d748ac9748f7f943eba3f18c2fb978486e
Joe Wigmar <joewigmar@gmail.com>
2025-10-27 13:22:25 -0300

    feat: create Home.jsx and import it in App.jsx

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Home from './Home'
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <div>
+      <Home />
+    </div>
   )
 }
 
---
+++ todolist/src/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div>Todo List</div>
+    )
+}
+
+export default Home
