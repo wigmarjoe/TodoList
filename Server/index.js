399b886ceed56947e04cffac36062ed3c891a191
Joe Wigmar <joewigmar@gmail.com>
2025-10-27 18:41:11 -0300

    connect to MongoDB and create initial POST /add route

--- Server/index.js
+++ Server/index.js
@@ -6,6 +6,12 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+mongoose.connect('mongodb://127.0.0.1/:27017/test')
+
+app.post('/add', (req, res => {
+    const task = req.body.task;
+}))
+
 app.listen(3001, () => {
     console.log("Server is Running")
 })
