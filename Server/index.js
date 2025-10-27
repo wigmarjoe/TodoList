0bb4827d5813699c2dfe9860df7a285fd82935ee
Joe Wigmar <joewigmar@gmail.com>
2025-10-27 19:40:48 -0300

    fix: adjust MongoDB connection URL to local instance

--- Server/index.js
+++ Server/index.js
@@ -7,15 +7,15 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect('mongodb://127.0.0.1/:27017/test')
+mongoose.connect('mongodb://127.0.0.1:27017/todo_db')
 
-app.post('/add', (req, res => {
+app.post('/add', (req, res) => {
     const task = req.body.task;
     TodoModel.create({
         task: task
     }).then(result => res.json(result))
     .catch(err => res.json(err))
-}))
+})
 
 app.listen(3001, () => {
     console.log("Server is Running")
