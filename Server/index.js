ecefade2147648f0dc511965bcf09ca83e003811
Joe Wigmar <joewigmar@gmail.com>
2025-10-27 18:59:44 -0300

    feat: create Todo model and implement task creation in POST /add route

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const TodoModel = require('./Models/Todo')
 
 const app = express()
 app.use(cors())
@@ -10,6 +11,10 @@ mongoose.connect('mongodb://127.0.0.1/:27017/test')
 
 app.post('/add', (req, res => {
     const task = req.body.task;
+    TodoModel.create({
+        task: task
+    }).then(result => res.json(result))
+    .catch(err => res.json(err))
 }))
 
 app.listen(3001, () => {
