6cc78cd598915e5588bbdabc4de224879bf4179f
Joe Wigmar <joewigmar@gmail.com>
2025-10-27 21:11:35 -0300

    feat: implement task completion toggle - add 'done' field to Todo model - create PUT /update/:id route to mark tasks as done - add handleEdit function and onClick checkbox in Home.jsx - update CSS for task layout and icons

--- todolist/src/Home.jsx
+++ todolist/src/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Create from "./Create";
 import { useEffect } from "react";
+import { Bs0CircleFill, BsFill0CircleFill, BsFillTrash2Fill } from 'react-icons/bs'
 
 function Home() {
     const [todos, setTodos] = useState([])
@@ -9,18 +10,32 @@ function Home() {
         .then(result => setTodos(result.data))
         .catch(err => console.log(err))
     }, [])
+
+    const handleEdit = (id) => {
+        axios.put(`http://localhost:3001/update/${id}`)
+        .then(result => console.log(result))
+        .catch(err => console.log(err))
+    }
+
     return (
         <div className='home'>
             <h2>Todo List</h2>
             <Create />
+            <br />
             {
                 todos.length === 0 
                 ?
                 <div><h2>No Record</h2></div>
                 :
                 todos.map(todo => (
-                    <div className="task">
-                        {todo.task}
+                    <div className='task'>
+                        <div className='checkbox' onClick={() => handleEdit(todo._id)}>
+                            <BsFillCircleFill className='icon'/>
+                            <p>{todo.task}</p>                
+                        </div>
+                        <div>
+                            <span><BsFillTrashFill className='icon'/></span>
+                        </div>
                     </div>
             
                 ))
