94623a72c2de788995859d20c64eebdedf111630
Joe Wigmar <joewigmar@gmail.com>
2025-10-29 09:06:19 -0300

    feat: add task deletion functionality

--- Server/index.js
+++ Server/index.js
@@ -22,6 +22,13 @@ app.put('/update/:id', (req, res) => {
     .catch(err => res.json(err))
 })
 
+app.delete('/delete/:id', (req, res) => {
+    const {id} = req.params;
+    TodoModel.findByIdAndDelete({_id: id})
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
 app.post('/add', (req, res) => {
     const task = req.body.task;
     TodoModel.create({
